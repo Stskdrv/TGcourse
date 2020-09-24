a92679e1a1673de8c95e450b098461d4ff84a816
Stas <Stanisalv.kad@gmail.com>
2020-09-24 20:26:05 +0300

    Homework2

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let firstRow = 'мама мыла раму';
+let secondRow = 'собака друг человека';
+
+if ((firstRow.split("а").length - 1)>(secondRow.split("а").length - 1)){
+    console.log(firstRow);
+
+} else {
+    console.log(secondRow); 
+}
+    
+
+
+// console.log(firstRow.split("а").length - 1); // мама мыла раму
