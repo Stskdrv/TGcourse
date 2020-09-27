4de619c3578b4a48a0fbc3d60672ae62a233ba85
Stas <Stanisalv.kad@gmail.com>
2020-09-27 15:54:32 +0300

    JS add

---
+++ js/main.js
@@ -0,0 +1,55 @@
+const $btn = document.getElementById('btn-kick');
+
+const character = {
+    name: 'Pikachu',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-character'),
+    elProgressbar: document.getElementById('progressbar-character'),
+}
+
+const enemy = {
+    name: 'Charmander',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-enemy'),
+    elProgressbar: document.getElementById('progressbar-enemy'),
+}
+
+$btn.addEventListener('click', function () {
+    console.log('Kick');
+    
+    changeHP(random(20), character);
+    changeHP(random(20), enemy);
+
+
+});
+
+function init() {
+    console.log('Start Game!');
+    renderHP(character);
+    renderHP(enemy);
+}
+function renderHP(person) {
+    renderHPLife(person);
+    renderProgressbarHP(person);
+
+}
+function renderHPLife(person) {
+    person.elHP.innerText = person.damageHP + ' / '+person.defaultHP;
+}
+function renderProgressbarHP(person) {
+    person.elProgressbar.style.width = person.damageHP + '%';
+}
+function changeHP(count, person) {
+    person.damageHP -= count;
+    renderHP(person);
+
+}
+function random(num) {
+    console.log(Math.random());
+    return Math.ceil(Math.random() * num);
+
+}
+
+init();
